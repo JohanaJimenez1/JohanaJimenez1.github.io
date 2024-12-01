be58dc84a7119f632f00352919c16a0a6da6f95b
JohanaJimenez1 <maried.jimenez11@gmail.com>
2024-12-01 13:35:43 +0100

    actualisation menu

---
+++ portfolio.js
@@ -0,0 +1,15 @@
+let menuVisible = false;
+
+function noneOrblock(){
+    if(menuVisible){
+        document.getElementById("nav").classList = "";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList = "responsive";
+        menuVisible = true;
+    }
+}
+function select(){
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+}
